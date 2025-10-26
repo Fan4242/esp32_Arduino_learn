bffbd48e577de2fbab7fef2e8495337f2aebb356
whatcanisay <15724180855@163.com>
2025-10-27 03:03:50 +0800

    v2 & v3 & v4

--- esp32_arduino_torch/src/main.cpp
+++ esp32_arduino_torch/src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// 定义外部中断的Mode
+// 0: 无中断，读取Touch值
+// 1：Touch中断，执行 TouchEvent()
+// 2: 外部IO的中断
+#define EXT_ISR_MODE 2
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+uint8_t stateLED = 1;
+
+void TouchEvent()
+{
+    Serial.printf("Touch Event.\r\n");
+    //stateLED = stateLED^1;
+    digitalWrite(15,stateLED);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void PinIntEvent()
+{
+    Serial.printf("PinInt Event.\r\n");
+    digitalWrite(15,stateLED);
+    printf("v4 _test");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setup()
+{
+    // put your setup code here, to run once:
+    pinMode(15, OUTPUT);
+    Serial.begin(115200);
+
+#if 1 == EXT_ISR_MODE
+    // Pin: T0(GPIO4), 函数指针:TouchEvent, 阈值: 40
+    touchAttachInterrupt(2, TouchEvent, 40);
+
+#elif 2 == EXT_ISR_MODE
+    pinMode(2, INPUT_PULLUP);
+    attachInterrupt(2, PinIntEvent, FALLING);
+
+#endif
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+
+#if 0 == EXT_ISR_MODE
+    Serial.printf("touch:%d\r\n", touchRead(T0));
+#endif
+
+    delay(200);
 }
